5d510768eccc6d980d2cb7088d43dde9e4116d8d
liu-dazhi <57987297+liu-dazhi@users.noreply.github.com>
2023-07-22 23:17:46 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://8.130.120.167:8080', // 将请求代理到后端服务器的地址
+                changeOrigin: true, // 设置为 true，以便将主机头的来源更改为目标 URL
+                pathRewrite: {
+                    '^/api': '', // 如果后端接口没有使用统一的前缀路径，可以将此处的 '/api' 替换为实际的前缀路径
+                },
+            },
+        },
+    },
+};
